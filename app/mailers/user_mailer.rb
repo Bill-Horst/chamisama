304212e905b1d9481dc47b65e4aaf5dff1ea12ac
Bill Horst <bill@billhorst.com>
2017-12-06 08:26:42 -0700

    fixed bug in email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def contact_form(email, name, message)
     @message = message
-    mail(from: email,to: 'bill@billhorst.com.com',subject: "A new contact form message from #{name}")
+    mail(from: email,to: 'bill@billhorst.com',subject: "A new contact form message from #{name}")
   end
 
 end
