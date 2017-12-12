bfaf838bdc08269ab497bc4e95c48d3715fdabbb
Bill Horst <bill@billhorst.com>
2017-12-11 17:14:26 -0700

    5.8 added raty star review and styled comment form

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,3 @@
 $( document ).on('turbolinks:load', function() { // specifically for Rails 5 (require in application.js)
   $('.custom-notice').delay(3000).fadeOut();
-  $('.rating').raty();
 });
--- app/models/product.rb
+++ app/models/product.rb
@@ -23,4 +23,8 @@ class Product < ApplicationRecord
     comments.rating_desc.last
   end
 
+  def average_rating
+    comments.average(:rating).to_f
+  end
+
 end
