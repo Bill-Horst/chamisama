0f2c4385905e4e916519e09e902fcff950a486b4
Bill Horst <bill@billhorst.com>
2017-12-16 11:42:49 -0700

    added automatic email on sign up functionality

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,9 @@ class UserMailer < ApplicationMailer
     mail(from: email,to: 'bill@billhorst.com',subject: "A new contact form message from #{name}")
   end
 
+  def welcome(user)
+    @appname = "Chamisama"
+    mail(to: user.email, subject: "Welcome to #{@appname}!")
+  end
+
 end
