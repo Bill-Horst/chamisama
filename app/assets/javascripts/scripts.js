52da8ca287074e8c5ee77819c462ecb27c52a6a2
Bill Horst <bill@billhorst.com>
2017-12-29 22:47:33 -0700

    improved landing page ui

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,3 +1,3 @@
 $( document ).on('turbolinks:load', function() { // specifically for Rails 5 (require in application.js)
-  $('.custom-notice').delay(3000).fadeOut();
+    $('.custom-notice').delay(3000).fadeOut('slow');
 });
