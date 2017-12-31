6cd8dfb75a275b84a780898a770018fff235cce4
Bill Horst <bill@billhorst.com>
2017-12-31 01:03:37 -0700

    added longer descriptions to products and displayed them on product show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171221045247) do
+ActiveRecord::Schema.define(version: 20171231071844) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171221045247) do
     t.decimal "price"
     t.string "country"
     t.integer "price_in_pennies"
+    t.text "story"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,8 @@ Product.create(
       image_url: 'gmcha.jpg',
       colour: 'green',
       country: 'Japan',
-      price_in_pennies: '4599'
+      price_in_pennies: '4599',
+      story: 'Genmaicha (玄米茶, "brown rice tea"), also called brown rice green tea, is the Japanese name for green tea combined with roasted brown rice. It is sometimes referred to colloquially as "popcorn tea" because a few grains of the rice pop during the roasting process and resemble popcorn. Although today it is consumed by all segments of society, this type of tea was originally drunk by poor Japanese, as the rice served as a filler and reduced the price of the tea, which is why it is also known as the "people\'s tea".'
     },
     {
       name: 'Oolong Tea',
@@ -25,7 +26,8 @@ Product.create(
       image_url: 'ooloncha.jpg',
       colour: 'black',
       country: 'China',
-      price_in_pennies: '1999'
+      price_in_pennies: '1999',
+      story: 'Oolong ( /ˈuːlɒŋ/) (simplified Chinese: 乌龙; traditional Chinese: 烏龍; pinyin: wūlóng) is a traditional Chinese tea (Camellia sinensis) produced through a process including withering the plant under strong sun and oxidation before curling and twisting. Most oolong teas, especially those of fine quality, involve unique tea plant cultivars that are exclusively used for particular varieties. The degree of oxidation can range from 8–85%, depending on the variety and production style.'
     },
     {
       name: 'Houjicha',
@@ -33,7 +35,8 @@ Product.create(
       image_url: 'hjcha.jpg',
       colour: 'green',
       country: 'Japan',
-      price_in_pennies: '2199'
+      price_in_pennies: '2199',
+      story: 'Hōjicha (ほうじ茶) is a Japanese green tea. It is distinctive from other Japanese green teas because it is roasted in a porcelain pot over charcoal, whereas most Japanese teas are steamed. The tea is fired at a high temperature, altering the leaf color tints from green to reddish brown. The process was first performed in Kyoto, Japan, in the 1920s and its popularity persists today.'
     },
     {
       name: 'Brewer',
@@ -41,7 +44,8 @@ Product.create(
       image_url: 'brewer.jpg',
       colour: '',
       country: '',
-      price_in_pennies: '6999'
+      price_in_pennies: '6999',
+      story: 'A kyūsu (急須) is a traditional Japanese teapot mainly used for brewing green tea.The common misconception is that a kyūsu always has a side handle. However, the word "kyūsu" merely means "teapot", even though in common usage kyūsu usually does refer to a teapot with a side handle. The two most common types of kyūsu are yokode kyūsu (横手急須 side hand(le) teapot), which has a side handle and which is the more common type, and ushirode kyūsu (後手急須 back hand(le) teapot), which has a rear handle, just like teapots in other parts of the world.'
     },
     {
       name: 'Imperial Dragon',
@@ -49,7 +53,8 @@ Product.create(
       image_url: 'imp_drag.jpg',
       colour: 'green',
       country: 'China',
-      price_in_pennies: '6999'
+      price_in_pennies: '6999',
+      story: 'Native to the shores of West Lake near Hangzhou, the old emperor city, this legendary and prized green tea unfolds with a great, flower-like bouquet underscored by notes of fresh cinnamon buns. The “Dragonwell” Long Jing appeals with a wonderfully smooth, full-bodied taste. It\'s wonderful to drink while watching the sunset with your cat.'
     },
     {
       name: 'Gyokuro Powder',
@@ -57,7 +62,8 @@ Product.create(
       image_url: 'gyokuropowder.jpg',
       colour: 'green',
       country: 'Japan',
-      price_in_pennies: '9999'
+      price_in_pennies: '9999',
+      story: 'Gyokuro (Japanese: 玉 露, "jade dew") is a type of shaded green tea from Japan. It differs from the standard sencha (a classic unshaded green tea) in being grown under the shade rather than the full sun. Gyokuro also differs from another shaded tea called kabusecha (lit., "covered tea"), in the length of time it undergoes the final growth under the shade (gyokuro is shaded for approximately three weeks, while kabusecha is shaded for approximately one week). The name "gyokuro" translates as "jewel dew" (or "jade dew", referring to the pale green colour of the infusion).'
     },
     {
       name: 'Ryokucha',
@@ -65,7 +71,8 @@ Product.create(
       image_url: 'ryokucha.jpg',
       colour: 'green',
       country: 'Japan',
-      price_in_pennies: '2399'
+      price_in_pennies: '2399',
+      story: 'Green tea (緑茶) is a type of tea that is made from Camellia sinensis leaves that have not undergone the same withering and oxidation process used to make oolong and black tea. Green tea originated in China, but its production has spread to many countries in Asia.Several varieties of green tea exist, which differ substantially because of the variety of C. sinensis used, growing conditions, horticultural methods, production processing, and time of harvest.'
     },
     {
       name: 'Shizuoka Black Tea',
@@ -73,7 +80,8 @@ Product.create(
       image_url: 'shizu_black.jpg',
       colour: 'black',
       country: 'Japan',
-      price_in_pennies: '3499'
+      price_in_pennies: '3499',
+      story: 'While 99% of tea produced in Japan is green tea, this Japanese black tea breaks from tradition. It is black, not green. There was no Wikipedia article to copy and paste about this tea so this writer had a choice: to make stuff up, or just type randomly until there was just about the right number of characters to neatly fill the panel for this page. He chose the latter.'
     },
     {
       name: 'Sanpincha',
@@ -81,7 +89,8 @@ Product.create(
       image_url: 'sanpin.jpg',
       colour: 'green',
       country: 'Japan',
-      price_in_pennies: '2499'
+      price_in_pennies: '2499',
+      story: 'Sanpin-cha (さんぴん茶) is the most famous jasmine tea originating in Okinawa.  Sanpin-cha is considered Chinese-style tea, and it has a lovely fragrance.  On the mainland it is usually labelled ジャスミン茶 (Jasmine Tea). Typically it also less caffeine than regular tea. It has been a favorite in Okinawa since the days of the Ryukyu Kingdom. Originating with the trade between the Ryukyu Kingdom and China, the tea is an example of the mixture of Chinese and Japanese influence in the islands.'
     },
     {
       name: 'Dianhong',
@@ -89,7 +98,8 @@ Product.create(
       image_url: 'dianhong.jpg',
       colour: 'black',
       country: 'China',
-      price_in_pennies: '3999'
+      price_in_pennies: '3999',
+      story: 'Dianhong tea (Chinese: 滇紅茶; pinyin: Diān hóng chá; literally: "Dian Lake red tea"; pronounced [tjɛ́n xʊ̌ŋ ʈʂʰǎ]) is a type of relatively high-end, gourmet Chinese black tea sometimes used in various tea blends and grown in Yunnan Province, China. The main difference between Dianhong and other Chinese black teas is the amount of fine leaf buds, or "golden tips," present in the dried tea. Dianhong teas produces a brew that is brassy golden orange in colour with a sweet, gentle aroma and no astringency.'
     },
     {
       name: 'Teacup Set',
@@ -97,7 +107,8 @@ Product.create(
       image_url: 'teacups.jpg',
       colour: '',
       country: '',
-      price_in_pennies: '3999'
+      price_in_pennies: '3999',
+      story: 'This teacup set enables the user to drink tea, and only tea, at any time of the day the user wishes. There are four of them, which means the user can either drink four different kinds of tea at once, or the same kind of tea in succession without having to wash the teacup before drinking again. When choosing which path to take, it is best the user not decide for himself / herself. Such a decision is best left to a professional.'
     }
   ]
 )
