bc98b57a410b8c9bd773ca0dac00e32fa1b798fb
Bill Horst <bill@billhorst.com>
2017-12-31 19:25:05 -0700

    added link to order show page on order index page and added link to product on order show page

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     user ||= User.new
     can :manage, User, id: user.id
-    can :read, Order, id: user.id
+    can :read, Order, user_id: user.id
 
     if user.admin?
       can :delete
