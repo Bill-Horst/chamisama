2ad9fca1a35ccac0807251ad57a94e305071bb49
Bill Horst <bill@billhorst.com>
2018-01-08 17:36:22 -0700

    refactored product viewed times

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,7 @@ class ProductsController < ApplicationController
   def show
     # @comments = @product.comments.order("created_at DESC")
     @comments = @product.comments.order('created_at DESC').paginate(:page => params[:page], :per_page => 2)
+    @product.viewed!
   end
 
   # GET /products/new
